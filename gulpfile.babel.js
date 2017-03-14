53e02fb4b738afb3e61c25c1327b349a123954c8
Jason Gray <jasonthomasgray@gmail.com>
2017-03-14 21:05:59 +0000

    add gulpfile watch. basic data loading

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,38 @@
 import gulp from 'gulp';
-import sass from 'gulp-sass';
+import pump from 'pump';
+const plugins = require('gulp-load-plugins')();
 
-gulp.task('scripts', () => {
-
+gulp.task('scripts', (done) => {
+  pump([
+    gulp.src([
+      'bower_components/angular/angular.js',
+      'src/app.js',
+    ]),
+    plugins.babel(),
+    plugins.concat('app.js'),
+    gulp.dest('dist'),
+  ], done)
 })
 
-gulp.task('static', () => {
-  return gulp.src('src/index.html')
-    .pipe(gulp.dest('dist'));
+gulp.task('static', (done) => {
+  pump([
+    gulp.src('src/index.html'),
+    gulp.dest('dist'),
+  ], done);
 })
 
-gulp.task('sass', () => {
-  return gulp.src('src/app.scss')
-    .pipe(sass({includePaths: ['.', 'bower_components/']}))
-    .pipe(gulp.dest('dist'));
+gulp.task('sass', (done) => {
+  pump([
+    gulp.src('src/app.scss'),
+    plugins.sass({includePaths: ['.', 'bower_components/']}),
+    gulp.dest('dist'),
+  ], done);
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/app.js', ['scripts']);
+  gulp.watch('src/index.html', ['static']);
+  gulp.watch('src/app.scss', ['sass']);
 });
 
 gulp.task('default', ['scripts', 'sass', 'static']);
